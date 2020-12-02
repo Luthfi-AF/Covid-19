97a8a13f65a74a859bb92b8ba704b7ab49ddb1f1
Dicky-dzul <72434002+Dicky-dzul@users.noreply.github.com>
2020-12-02 11:50:57 +0700

    Rujukan

---
+++ lib/DetailRujukan.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title:"Detail Rujukan",
+        home:DetailRujukan(),
+    );
+  }
+}
+
+class DetailRujukan extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            leading: Icon(Icons.arrow_back_rounded,color:Colors.white,size: 30.0),
+        ),
+        body:Container(
+            margin: EdgeInsets.all(20),
+            child: Column(
+                children: <Widget>[
+                    Card(
+                        margin: EdgeInsets.only(bottom: 20),
+                        elevation:5,
+                        child:Column(
+                            children: <Widget>[
+                                Container(
+                                    child: Image(
+                                        image: AssetImage("assets/rs-mitra-keluarga-mitrakeluarga.com_ratio-16x9.jpg"),
+                                        fit: BoxFit.cover,
+                                    ),
+                                ),
+                            ],
+                        ),
+                    ),
+                    Row(
+                        children: <Widget>[
+                            Image.asset("assets/gedung.png",
+                            height: 30.0,
+                            width: 30.0,),
+                            Text(
+                                "   Rs A",
+                                style: TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold,
+                                ),
+                            ),
+                        ],
+                    ),
+                    Row(
+                        children: <Widget>[
+                            Text(
+                                "Informasi Rs A",
+                                style: TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold,
+                                ),
+                            ),
+                        ],
+                    ),
+                    Row(
+                        children: <Widget>[
+                            Icon(Icons.local_phone,color: Colors.black, size: 25.0,),
+                            Text(
+                                "   0987654321",
+                                style: TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold,
+                                ),
+                            ),
+                        ],
+                    ),
+                    Row(
+                        children: <Widget>[
+                            Icon(Icons.location_on_sharp,color: Colors.black, size: 25.0,),
+                            Text(
+                                "   Bandung",
+                                style: TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold,
+                                ),
+                            ),
+                        ],
+                    ),
+                ],
+            ),
+        ),
+    );
+  }
+}
---
+++ lib/Rujukan.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Rujukan",
+        home: Rujukan(),
+    );
+  }
+}
+
+class Rujukan extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            leading: Icon(Icons.arrow_back_rounded,color:Colors.white,size: 30.0),
+        ),
+        body: Container(
+            margin: EdgeInsets.all(20),
+            child: ListView(
+                children: <Widget>[
+                    Card(
+			            margin: EdgeInsets.only(bottom: 10),
+                        elevation:5,
+                        child:Column(
+                            children: <Widget>[
+                                Container(
+                                    child: Image(
+                                        image: AssetImage("assets/rumahsakitrujukan.jpg"),
+                                        fit: BoxFit.cover,
+                                    ),
+                                ),
+                            ],
+                        ),
+                    ),
+                    Card(
+                        elevation:5,
+                        child:Row(
+                            children: <Widget>[
+                                Container(margin:EdgeInsets.all(5),
+                                child: Icon(Icons.local_hospital,size: 45.0),),
+                                Text("Rs A",
+                                style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold,
+                                ),),
+                            ],
+                        ),
+                    ),
+                ],
+            ),
+        ),
+    );
+  }
+}
